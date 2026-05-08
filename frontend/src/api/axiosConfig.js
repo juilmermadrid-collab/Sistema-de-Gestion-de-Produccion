10a7a349a8c30c2af3321147ee7942d0b7c51c12
Jeferson Caamaño <chiskitogarcia@gmail.com>
2026-05-08 10:52:43 -0500

    fix: resolver conflicto en axiosConfig.js

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-});
-
-=======
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
->>>>>>> b6f21f12907c6b74880c37a26e2130ebe4ebec93
 export default api;
